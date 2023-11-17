5203eb3baf62de63c461559bec2eb775f2a4dcb3
LariXv1 <berkaysagdic@hotmail.com>
2023-11-17 14:49:03 +0300

    Sahneler arası kararma
    Portala girip diğer sahneye girerken ekran kararıyor diğer sahneye girince açılıyor. Her şey Berkay klasöründe.

--- GameJam/Assets/Berkay/Scripts/PortalEnter.cs
+++ GameJam/Assets/Berkay/Scripts/PortalEnter.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PortalEnter : MonoBehaviour
 {
+
+    public float waitToLoad = 1f;
+    private bool shouldLoadAfterFade;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +18,23 @@ public class PortalEnter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (shouldLoadAfterFade)
+        {
+            waitToLoad -= Time.deltaTime;
+            if (waitToLoad < 0f)
+            {
+                shouldLoadAfterFade = false;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            shouldLoadAfterFade = true;
+            UIFade.instance.FadeToBlack();
         }
     }
 }
