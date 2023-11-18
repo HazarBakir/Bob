7d5a0f49461b863eafb0918485a258a9eacca5f7
alimertcetin <mrtmert54@gmail.com>
2023-11-18 22:14:57 +0300

    feat: Code generation update

--- GameJam/Assets/Scripts/CodeGeneration/AnimationConstants.cs
+++ GameJam/Assets/Scripts/CodeGeneration/AnimationConstants.cs
@@ -5,4 +5,64 @@ using UnityEngine;
 
 public static class AnimationConstants
 {
+	// Inner Classes
+	public static class Canvas
+	{
+		// Inner Classes
+		public static class Clips
+		{
+			// Members
+			public const string Canvas_WakeUpBlackScreenAnim = "WakeUpBlackScreenAnim";
+			public static readonly int Canvas_WakeUpBlackScreenAnimHash = Animator.StringToHash("WakeUpBlackScreenAnim");
+			
+		} // class Clips
+		
+		public static class Layers
+		{
+			// Members
+			public const int Canvas_Base_Layer = 0;
+			
+		} // class Layers
+		
+		
+	} // class Canvas
+	
+	public static class PlayerController
+	{
+		// Inner Classes
+		public static class Clips
+		{
+			// Members
+			public const string PlayerController_Idle = "Idle";
+			public static readonly int PlayerController_IdleHash = Animator.StringToHash("Idle");
+			public const string PlayerController_Sprint = "Sprint";
+			public static readonly int PlayerController_SprintHash = Animator.StringToHash("Sprint");
+			public const string PlayerController_Walking = "Walking";
+			public static readonly int PlayerController_WalkingHash = Animator.StringToHash("Walking");
+			public const string PlayerController_Jump = "Jump";
+			public static readonly int PlayerController_JumpHash = Animator.StringToHash("Jump");
+			
+		} // class Clips
+		
+		public static class Parameters
+		{
+			// Members
+			public const string PlayerController_IsWalking_Bool = "IsWalking";
+			public static readonly int PlayerController_IsWalking_BoolID = Animator.StringToHash("IsWalking");
+			public const string PlayerController_IsJumping_Bool = "IsJumping";
+			public static readonly int PlayerController_IsJumping_BoolID = Animator.StringToHash("IsJumping");
+			
+		} // class Parameters
+		
+		public static class Layers
+		{
+			// Members
+			public const int PlayerController_Base_Layer = 0;
+			
+		} // class Layers
+		
+		
+	} // class PlayerController
+	
+	
 } // class AnimationConstants
